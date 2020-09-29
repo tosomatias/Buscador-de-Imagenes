1511fd5bc03706e0813f30513d182a589fa5aca1
matute <tosomatias@gmail.com>
2020-09-29 20:54:19 -0300

    Updates

---
+++ precache-manifest.e06f35c65c23173648e2b347bd505bc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c311aeefd0855304fe18dc632c91a9",
+    "url": "/Buscador-de-Imagenes/index.html"
+  },
+  {
+    "revision": "4d4d3251134a05fb9d7b",
+    "url": "/Buscador-de-Imagenes/static/js/2.bba10083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Buscador-de-Imagenes/static/js/2.bba10083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6bd171b3c9d9c4bd7c",
+    "url": "/Buscador-de-Imagenes/static/js/main.d6f96674.chunk.js"
+  },
+  {
+    "revision": "8a66472b2089e836482c",
+    "url": "/Buscador-de-Imagenes/static/js/runtime-main.4e72a2e6.js"
+  }
+]);
